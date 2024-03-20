3a3dc62cbef07bf985327a30c0aa85810f0bd703
GeorgeDenis <george.duluta@yahoo.com>
2024-03-20 21:38:38 +0200

    fixes

--- HealthHub/Frontend/src/layouts/Auth.jsx
+++ HealthHub/Frontend/src/layouts/Auth.jsx
@@ -5,7 +5,6 @@ import {
   UserPlusIcon,
   ArrowRightOnRectangleIcon,
 } from "@heroicons/react/24/solid";
-import { Navbar, Footer } from "@/widgets/layout";
 import routes from "@/routes";
 
 export function Auth() {
--- HealthHub/Frontend/src/widgets/layout/index.js
+++ HealthHub/Frontend/src/widgets/layout/index.js
@@ -1,4 +1,2 @@
 export * from "@/widgets/layout/Sidenav";
 export * from "@/widgets/layout/DashboardNavbar";
-export * from "@/widgets/layout/Footer";
-export * from "@/widgets/layout/Navbar";
